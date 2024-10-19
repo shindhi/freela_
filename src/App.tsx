cfd452ffd1a4f44052007b4d6e0e3238e2aea28f
Shindhi <diogoshindhinaka@gmail.com>
2024-10-19 19:33:24 -0300

    Add os componentes de Input e Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,66 +3,80 @@ import './index.css'
 import { Power, PowerOff } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
 
+import * as Input from './components/Input'
+import { Button } from './components/button'
+
 export function App() {
   return (
     <>
-      <div className="min-h-screen bg-zinc-950 grid grid-cols-app p-4 gap-3">
-        <div className="flex flex-col justify-between gap-3 bg-zinc-900 rounded-2xl">
+      <div className="grid min-h-screen grid-cols-app gap-4 bg-zinc-950 p-4">
+        <div className="flex flex-col justify-between gap-3 rounded-2xl bg-zinc-900">
           <div className="flex justify-center p-4">
-            <span className="text-white font-semibold text-5xl w-full text-center">
+            <span className="w-full text-center text-5xl font-semibold text-white">
               00:00
             </span>
 
-            <div className="flex gap-4 ml-auto">
-              <button className="bg-zinc-600 rounded-lg p-6 text-white">
+            <div className="ml-auto flex gap-4">
+              <Button
+                className="border-green-600 text-green-600 transition hover:bg-green-600/30"
+                variant="outline"
+              >
                 <Power strokeWidth={2} size={28} />
-              </button>
+              </Button>
 
-              <button className="bg-zinc-600 rounded-lg p-6 text-white">
+              <Button
+                className="border-red-600 text-red-600 transition hover:bg-red-600/30"
+                variant="outline"
+              >
                 <PowerOff strokeWidth={2} size={28} />
-              </button>
+              </Button>
             </div>
           </div>
 
-          <div className="p-4 flex gap-3 justify-between">
-            <div className="h-24 w-24 bg-white rounded"></div>
-            <div className="h-24 w-24 bg-white rounded"></div>
-            <div className="h-24 w-24 bg-white rounded"></div>
+          <div className="flex justify-between gap-3 p-4">
+            <div className="h-24 w-24 rounded bg-white"></div>
+            <div className="h-24 w-24 rounded bg-white"></div>
+            <div className="h-24 w-24 rounded bg-white"></div>
           </div>
         </div>
 
-        <div className="bg-zinc-900 rounded-2xl p-4">
-          <form className="flex flex-col gap-4">
-            <div className="text-white flex flex-col gap-1 w-full">
-              <label className="font-semibold" htmlFor="test1">
-                Teste
-              </label>
-              <input
-                type="text"
-                placeholder="teste"
-                id="test1"
-                className={twMerge(
-                  'rounded-lg py-1 px-2 placeholder:text-zinc-500 bg-white/5 border border-white/10 outline-none focus:ring-0',
-                  'focus:border-blue-500 focus:ring-1 focus:ring-blue-500',
-                )}
-              />
-            </div>
+        <div className="rounded-2xl bg-zinc-900 p-4">
+          <form className="flex flex-col gap-5">
+            <Input.Root>
+              <Input.Label htmlFor="step1">Passo 1 [m]</Input.Label>
+              <Input.Control type="number" id="step1" placeholder="Ex: 0" />
+            </Input.Root>
+
+            <Input.Root>
+              <Input.Label htmlFor="step2">Passo 2 [s]</Input.Label>
+              <Input.Control type="number" id="step2" placeholder="Ex: 0" />
+            </Input.Root>
+
+            <Input.Root>
+              <Input.Label htmlFor="step3">Passo 3 [m]</Input.Label>
+              <Input.Control type="number" id="step3" placeholder="Ex: 0" />
+            </Input.Root>
+
+            <Input.Root>
+              <Input.Label htmlFor="step4">Temp. Max °C</Input.Label>
+              <Input.Control type="number" id="step4" placeholder="Ex: 0" />
+            </Input.Root>
           </form>
         </div>
 
-        <div className="col-span-full bg-zinc-900 rounded-2xl grid grid-cols-6 p-4">
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
-          <div className="h-24 w-24 bg-white rounded self-center justify-self-center"></div>
+        <div className="col-span-full grid grid-cols-6 rounded-2xl bg-zinc-900 p-4">
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
+          <div className="h-24 w-24 self-center justify-self-center rounded bg-white"></div>
         </div>
       </div>
     </>
