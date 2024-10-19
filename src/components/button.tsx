8c9d108445d703284f665e9356e7f64d73ebefa7
Shindhi <diogoshindhinaka@gmail.com>
2024-10-19 19:31:57 -0300

    Add component Button

---
+++ src/components/button.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
+
+const button = tv({
+  base: [
+    'rounded-lg p-6 outline-none',
+    'focus-visible:ring-2 focus-visible:ring-offset-2 ',
+  ],
+  variants: {
+    variant: {
+      outline: 'border-2',
+      primary: 'text-white',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+
+export type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return <button {...props} className={button({ variant, className })} />
+}
