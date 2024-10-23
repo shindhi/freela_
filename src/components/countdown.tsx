665823748e7939787ad8b9e5bafc310720a1f5d4
Shindhi <diogoshindhinaka@gmail.com>
2024-10-23 12:53:24 -0300

    Add component countdown

---
+++ src/components/countdown.tsx
@@ -0,0 +1,13 @@
+export function Countdown() {
+  return (
+    <>
+      <span className="rounded-lg bg-white/10 px-4 py-8">0</span>
+      <span className="rounded-lg bg-white/10 px-4 py-8">0</span>
+      <div className="flex w-16 items-center justify-center overflow-hidden py-8">
+        :
+      </div>
+      <span className="rounded-lg bg-white/10 px-4 py-8">0</span>
+      <span className="rounded-lg bg-white/10 px-4 py-8">0</span>
+    </>
+  )
+}
