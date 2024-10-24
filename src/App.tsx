9353c2dda1e6db49714b13e835de5123c1d99334
Shindhi <diogoshindhinaka@gmail.com>
2024-10-24 12:00:23 -0300

    add button save to layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export function App() {
     <>
       <div className="grid min-h-screen grid-cols-app gap-4 bg-zinc-950 p-4">
         <div className="flex flex-col justify-between gap-3 rounded-2xl bg-zinc-900">
-          <div className="flex justify-center p-6">
+          <div className="mt-6 flex justify-center p-6">
             <span className="w-full text-center text-7xl font-semibold text-white">
               00:00
             </span>
@@ -65,9 +65,45 @@ export function App() {
             </Input.Root>
 
             <Input.Root>
-              <Input.Label htmlFor="step4">Temp. Max °C</Input.Label>
+              <Input.Label htmlFor="step4">T. Max °C</Input.Label>
               <Input.Control type="number" id="step4" placeholder="Ex: 0" />
             </Input.Root>
+
+            <div className="flex flex-col gap-1 font-semibold">
+              <span className="text-white">Frequência</span>
+
+              <div className="flex justify-between gap-4">
+                <button
+                  className={twMerge(
+                    'flex flex-1 items-center justify-center rounded-lg border border-white/10 bg-white/5 py-2 text-white outline-none',
+                    'hover:border-white/20',
+                    '[data-state="checked"]:border-blue-500 [data-state="checked"]:bg-blue-500/30',
+                  )}
+                >
+                  25
+                </button>
+
+                <button
+                  className={twMerge(
+                    'flex flex-1 items-center justify-center rounded-lg border border-white/10 bg-white/5 py-2 text-white outline-none',
+                    'hover:border-white/20',
+                    '[data-state="checked"]:border-blue-500 [data-state="checked"]:bg-blue-500/30',
+                  )}
+                >
+                  60
+                </button>
+              </div>
+            </div>
+
+            <button
+              className={twMerge(
+                'mt-10 flex cursor-pointer items-center justify-center rounded-lg border border-blue-500 py-2 font-semibold text-white transition',
+                'enabled:hover:bg-blue-500/90 disabled:cursor-not-allowed disabled:border-blue-500/30 disabled:text-white/30',
+              )}
+              disabled={false}
+            >
+              Salvar
+            </button>
           </form>
         </div>
 
